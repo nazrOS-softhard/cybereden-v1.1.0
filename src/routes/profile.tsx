d36360533a169ab4c20e9874d6d8c9976664b7d5
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-05-24 16:24:41 +0300

    Update profile.tsx

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -107,7 +107,7 @@ function ProfilePage() {
       subtitle={t("profile.subtitle")}
     >
       {/* --- ТРЕУГОЛЬНИК В ЗЕЛЕНОЙ ОБЛАСТИ --- */}
-      <div className="absolute top-[200px] right-[900px] z-50 pointer-events-none scale-[2.0]">
+      <div className="absolute top-[200px] right-[800px] z-50 pointer-events-none scale-[2.0]">
         <div className="relative flex items-center justify-center w-5 h-5">
           <svg
             viewBox="0 0 100 100"
