efaf5eb1f543434758640717f1745a638108c77c
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-05-28 01:26:23 +0300

    Update supabaseClient.ts

--- backend/src/lib/supabaseClient.ts
+++ backend/src/lib/supabaseClient.ts
@@ -1,13 +1,10 @@
-
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
-// Параноидальная проверка на случай, если забыли добавить переменные окружения в Vercel
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Критическая ошибка: отсутствуют переменные SUPABASE_URL или SUPABASE_ANON_KEY в .env');
 }
 
-// Экспортируем чистый инстанс
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
