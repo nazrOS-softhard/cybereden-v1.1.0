ba7392a024bb4fb8b4218da5f14923c5a4635599
назрОС <102623319+nazrOS-softhard@users.noreply.github.com>
2026-05-27 18:04:01 +0300

    Update ExpandedCardModal.tsx

--- src/components/ExpandedCardModal.tsx
+++ src/components/ExpandedCardModal.tsx
@@ -112,13 +112,22 @@ export function ExpandedCardModal({
                     {/* Если есть streamUrl, показываем Twitch-плеер */}
                     {streamUrl ? (
                       <div className="aspect-video w-full bg-black rounded-lg overflow-hidden mt-4">
-             <iframe
-  src={`https://embed.twitch.tv?channel=${streamUrl.split('/').pop()}&parent=cybereden.vercel.app&autoplay=true`}
+            <iframe
+  src={`https://player.twitch.tv/?channel=${streamUrl.split('/').pop()}&parent=cybereden.vercel.app&autoplay=true`}
   width="100%"
   height="100%"
   allow="autoplay; fullscreen"
   allowfullscreen
   className="w-full h-full"
+  onLoad={() => {
+    // Попытка запустить плеер через JS
+    setTimeout(() => {
+      const iframe = document.querySelector('iframe');
+      if (iframe) {
+        iframe.contentWindow?.postMessage('{"method":"play","params":[]}', '*');
+      }
+    }, 100);
+  }}
 />
                       </div>
                     ) : (
